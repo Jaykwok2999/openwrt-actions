97f7c9d166e52636ce31a9fa7fda4cc9de9d8d6a
Jaykwok2999 <61024435@qq.com>
2025-07-06 21:46:35 +0800

    Update diy-openwrt.sh

--- diy/diy-openwrt.sh
+++ diy/diy-openwrt.sh
@@ -69,17 +69,6 @@ rm -rf feeds/small/{shadowsocksr-libev,shadowsocks-rust,luci-app-ssr-plus,luci-i
 rm -rf feeds/luci/applications/{shadowsocksr-libev,shadowsocks-rust,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-ssr-plus,luci-i18n-ssr-plus-zh-cn,luci-app-wol,luci-app-bypass,luci-app-argon-config,luci-theme-argon}
 rm -rf feeds/luci/packages/net/{shadowsocksr-libev-ssr-check,shadowsocksr-libev-ssr-local,shadowsocksr-libev-ssr-redir,shadowsocksr-libev-ssr-server}
 # 将packages源的相关文件替换成passwall_packages源的
-rm -rf feeds/istoreos_ipk/patch/wall-luci/luci-app-passwall
-rm -rf feeds/istoreos_ipk/geoview
-rm -rf feeds/third_party/luci-app-smartdns
-rm -rf feeds/third_party/smartdns
-rm -rf feeds/istoreos_ipk/op-mosdns
-rm -rf feeds/istoreos_ipk/xray-core
-rm -rf feeds/istoreos_ipk/sing-box
-rm -rf feeds/istoreos_ipk/chinadns-ng
-rm -rf feeds/istoreos_ipk/microsocks
-rm -rf feeds/istoreos_ipk/dns2socks
-rm -rf feeds/istoreos_ipk/ipt2socks
 rm -rf feeds/istoreos_ipk/patch/wall-luci/luci-app-vlmcsd
 rm -rf feeds/istoreos_ipk/patch/istoreos-24.10
 rm -rf feeds/istoreos_ipk/vlmcsd
@@ -92,16 +81,8 @@ rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/dns2socks
 rm -rf feeds/packages/net/dns2tcp
 rm -rf feeds/packages/net/microsocks
-cp -r feeds/small/xray-core feeds/packages/net
-cp -r feeds/small/mosdns feeds/packages/net
-cp -r feeds/small/v2ray-geodata feeds/packages/net
-cp -r feeds/small/v2ray-geoip feeds/packages/net
-cp -r feeds/small/sing-box feeds/packages/net
-cp -r feeds/small/chinadns-ng feeds/packages/net
-cp -r feeds/small/dns2socks feeds/packages/net
-cp -r feeds/small/ipt2socks feeds/packages/net
-cp -r feeds/small/microsocks feeds/packages/net
-cp -r feeds/small/geoview feeds/packages/net
+rm -rf feeds/third_party/luci-app-LingTiGameAcc
+rm -rf feeds/third_party/luci-app-socat
 
 # istoreos-theme
 rm -rf feeds/luci/themes/luci-theme-argon
@@ -109,8 +90,7 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/third/luci-theme-argon
 rm -rf feeds/third/luci-app-argon-config
 rm -rf feeds/istoreos_ipk/theme/luci-theme-argon
-rm -rf feeds/istoreos_ipk/theme/luci-app-argon-config
-
+rm -rf feeds/luci/applications/luci-app-dockerman
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -156,12 +136,16 @@ git_sparse_clone main https://github.com/Jaykwok2999/socat luci-app-socat
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-upnp
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages miniupnpd
 rm -rf feeds/small/ luci-app-OpenClash
-rm -rf feeds/istoreos_ipk/patch/wall-luci/luci-app-OpenClash
+rm -rf feeds/istoreos_ipk/luci-app-OpenClash
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-OpenClash
+git_sparse_clone main https://github.com/Jaykwok2999/socat luci-app-socat
+git_sparse_clone main https://github.com/Jaykwok2999/socat socat
+git_sparse_clone main https://github.com/Jaykwok2999/istoreos-theme luci-app-argon-config
+git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-dockerman
 
-# golong1.24.2依赖
+# golong1.25.x依赖
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 # SSRP & Passwall
 git clone https://github.com/Jaykwok2999/luci-app-passwall.git package/passwall -b main
